3a0d36018b28913988d48bd0e12f5d4673478f14
Christopher Willis <willis.christopher.a@gmail.com>
2019-06-12 16:12:21 -0500

    conditional render testting, ready for expansion

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import './App.css';
 import Navbar from "./containers/topNavContainer"
 
-function App() {
+function App(props) {
+  let conditionalRender = ""
+  if(props.current.cod === undefined){
+    conditionalRender= 
+    <p>
+      Enter Zip Code and press Enter
+    </p>
+  }else if(props.current.cod !== 200){
+
+  }
   return (
     <div className="App">
       <header className="App-header">
         <Navbar/>
-        <p>
-          Enter Zip Code and press Enter
-        </p>
+        {conditionalRender}
       </header>
     </div>
   );
