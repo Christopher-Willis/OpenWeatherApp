1bd86b3464c47a8bba437e3f718cba56db99c1f6
Christopher Willis <willis.christopher.a@gmail.com>
2019-06-12 12:48:32 -0500

    removed create react app junk added instrutions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from "./components/topNav"
+import Navbar from "./containers/topNavContainer"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar/>
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Enter Zip Code and press Enter
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
