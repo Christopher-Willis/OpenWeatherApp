35cdd58cb0122c03721be95552e917582c3dd9bf
Christopher Willis <willis.christopher.a@gmail.com>
2019-06-12 22:14:55 -0500

    Diet conditional Rendering functioning

--- src/App.js
+++ src/App.js
@@ -2,24 +2,48 @@ import React from 'react';
 import './App.css';
 import Navbar from "./containers/topNavContainer"
 
-function App(props) {
+class App extends React.Component {
+
+  
+
+  render() {
+
+
+  // will change to react router if I have time, conditional rendering is sub optimal
+  // given time I would like to have routes with /(zipcode) properly send
+  // fetch requests front side. My current button pushing version of the site doesn't
+  // do that. This is easier, though, and not in the spec (muhahaha)
   let conditionalRender = ""
-  if(props.current.cod === undefined){
+  if(this.props.current.cod === undefined){
     conditionalRender= 
     <p>
       Enter Zip Code and press Enter
     </p>
-  }else if(props.current.cod !== 200){
-
+  }else if(this.props.current.cod !== 200){
+    conditionalRender = 
+    <p>
+      Invalid Zip Code, try again
+    </p>
+  }else{
+    conditionalRender=
+    <p>
+      {this.props.current.weather[0].description}
+    </p>
   }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <Navbar/>
-        {conditionalRender}
       </header>
+      {conditionalRender}
     </div>
   );
+
+  }
+
+  
 }
 
 export default App;
