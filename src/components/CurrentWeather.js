8f856143906fb1964c6623b38f64045354984e89
Christopher Willis <willis.christopher.a@gmail.com>
2019-06-13 00:54:10 -0500

    current weather 95% functional, themes needed

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,85 +1,58 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { fade } from '@material-ui/core/styles/colorManipulator';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
 
-const styles = theme => ({
-  root: {
-    width: '100%',
-    position: 'fixed',
-    top: 0
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 345,
+    // backgroundColor:"#212121"
   },
-  grow: {
-    flexGrow: 1,
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
   },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
+  avatar: {
+    backgroundColor: "#9CCC65",
   },
-  title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing.unit * 2,
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing.unit * 3,
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    width: theme.spacing.unit * 9,
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-    width: '100%',
-  },
-  inputInput: {
-    paddingTop: theme.spacing.unit,
-    paddingRight: theme.spacing.unit,
-    paddingBottom: theme.spacing.unit,
-    paddingLeft: theme.spacing.unit * 10,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: 200,
-    },
-  },
-});
+}));
 
-class CurrentWeather extends React.Component {
-
-
-
-
-  render() {
-    const { classes } = this.props;
-
-
-
-    return ( null
-    )
-  }
+const kelvinToF = (number) =>{
+  return ((number - 273.15) * 9/5) + 32
 }
 
-CurrentWeather.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
 
-export default withStyles(styles)(CurrentWeather);
+export default function RecipeReviewCard(props) {
+  const classes = useStyles();
+  const timeOfReport = new Date(0)
+  timeOfReport.setUTCSeconds(props.current.dt)
+  const iconPicture = `http://openweathermap.org/img/w/${props.current.weather[0].icon}.png`
+  return (
+    <Card className={classes.card} >
+      <CardHeader
+        avatar={
+          <Avatar aria-label="Recipe" className={classes.avatar}>
+            <img src={iconPicture} alt="weather icon" height="42" width="42"/>
+          </Avatar>
+        }
+        titleTypographyProps={{variant:'h4' }}
+        title={parseInt(kelvinToF(parseInt(props.current.main.temp)))+"°F"}
+        subheader={props.current.weather[0].description}
+        subheaderTypographyProps={{variant:'h6' }}
+      />
+      <CardContent>
+        <Typography  variant="body1" color="textPrimary" component="p">
+          Hi/Lo: {parseInt(kelvinToF(parseInt(props.current.main.temp_max)))+"°F/"+parseInt(kelvinToF(parseInt(props.current.main.temp_min)))+"°F"}
+        </Typography>
+         {/* <Typography  variant="body1" color="textPrimary" component="p">
+          At: {timeOfReport.toLocaleString()}
+        </Typography> 
+        not  feature, so removed, but useful*/
+        }
+      </CardContent>
+    </Card>
+  );
+}
